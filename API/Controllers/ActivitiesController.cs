a725a6a9d5e06f76b76657f6d45749e06d440b1b
Luis Schweigard <luis.schweigard@hs-augsburg.de>
2020-01-25 16:08:48 +0100

    Finished Activity CRUD API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -14,18 +14,40 @@ namespace API.Controllers
     {
         private readonly IMediator _mediator;
 
-        public ActivitiesController(IMediator mediator){
+        public ActivitiesController(IMediator mediator)
+        {
             this._mediator = mediator;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> List(){
+        public async Task<ActionResult<List<Activity>>> List()
+        {
             return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> Details(Guid id) {
-            return await _mediator.Send(new Details.Query {Id = id});
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediator.Send(new Delete.Command { Id = id });
         }
     }
 }
